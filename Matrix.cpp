62557ca47542cbb2038dc40e31d5d88f9fb2d1a6
Hassan <phelenon@gmail.com>
2020-03-15 19:29:07 +0500

    intial commit

---
+++ Matrix.cpp
@@ -0,0 +1,40 @@
+#include "Matrix.h"
+#include <iostream>
+#include <stdexcept>
+#include <iomanip>
+
+using namespace std;
+
+Matrix::Matrix(int x, int y)
+  :xSize(x),ySize(y)
+{
+  mat=new float*[x];
+  for (int c=0;c<x;++c){
+    mat[c]=new float[y];
+  }
+};
+
+float& Matrix::getVal(int x,int y)
+{
+  //check if cell valid
+  if (x>=xSize || y>=ySize || x<0 || y<0)
+    throw invalid_argument("matrix out of bounds");
+
+  return mat[x][y];
+}
+
+ std::ostream& operator<<(std::ostream& os, Matrix const & m)
+ {
+   cout<<endl;
+   for (int i=0;i<m.xSize;++i){
+     cout<<'|';
+     for (int j=0;j<m.ySize;++j){
+       std::cout << std::setw(4);
+       cout<< m.mat[i][j] <<"  ";
+     }
+     cout<<'|';
+     cout<<endl;
+   }
+
+   return os;
+ }
---
+++ Matrix.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+
+class Matrix
+{
+public:
+  //constructor
+	Matrix(int x, int y);
+
+  //get value of cell by reference
+	float& getVal(int x, int y);
+
+  friend std::ostream& operator<<(std::ostream& os, Matrix const & m);
+private:
+	float** mat;
+	int xSize;
+	int ySize;
+};
